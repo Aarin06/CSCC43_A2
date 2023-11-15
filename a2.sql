3c7aa867257b45fd877c4c6589297c9faeef9718
Aarin06 <aarinjasikumar@gmail.com>
2023-11-14 21:14:48 -0500

    bug fixes

--- a2.sql
+++ a2.sql
@@ -8,19 +8,20 @@ SET search_path TO A2;
 CREATE VIEW InstructorNoPHDCount AS
 
 SELECT d.dcode, d.dname, count(*) as numOfInstructors
-FROM department d JOIN instructor i ON d.dcode = i.code
+FROM department d JOIN instructor i ON d.dcode = i.dcode
 WHERE idegree <> 'PhD'
-GROUP BY dcode, dname
+GROUP BY d.dcode, d.dname;
+
 
 INSERT INTO query1(dname)
 (SELECT d.dname
-FROM InstructorNoPHDCount
+FROM InstructorNoPHDCount d
 WHERE numOfInstructors = (
   SELECT MIN(numOfInstructors)
   FROM InstructorNoPHDCount)
-)
+);
 
-DROP VIEW InstructorNoPHDCount
+DROP VIEW InstructorNoPHDCount;
 	
 --Query 2
 CREATE VIEW csDcode AS
@@ -31,22 +32,17 @@ CREATE VIEW csDcode AS
 INSERT INTO query2 (num)
 	SELECT count(sid) as num
 	FROM student
-	WHERE yearofstudy = 2 and dcode in csDcode;
+	WHERE yearofstudy = 2 and dcode in (SELECT * FROM csDcode);
 
-DROP VIEW csDcode;	
 
---Query 3
-CREATE VIEW csDcode AS
-	SELECT dcode
-	FROM department
-	WHERE dname = 'Computer Science';
-	
+
+--Query 3	
 CREATE VIEW yearlyEnrollment AS
 	SELECT cs.year as year, count(sc.sid) as enrollmentCount
 	FROM courseSection cs
 	JOIN studentCourse sc
 		ON sc.csid = cs.csid
-	WHERE cs.dcode in csDcode AND cs.year BETWEEN 2016 AND 2020
+	WHERE cs.dcode in (SELECT * FROM csDcode) AND cs.year BETWEEN 2016 AND 2020
 	GROUP BY cs.year;
 
 INSERT INTO query3 (year, enrollment)
@@ -54,10 +50,9 @@ SELECT year, enrollmentCount as enrollment
 FROM yearlyEnrollment
 WHERE enrollmentCount = (
 	SELECT max(enrollmentCount)
-	FROM yearlyEnrollment;
-)
+	FROM yearlyEnrollment
+);
 
-DROP VIEW csDcode;
 DROP VIEW yearlyEnrollment;
 
 --Query 4
@@ -75,33 +70,31 @@ FROM courseSection cs
 WHERE year = (
 SELECT max(cs2.year)
 FROM courseSection cs2)
-GROUP BY year
+GROUP BY year;
     
 CREATE VIEW AvgGrades AS 
 SELECT sc.sid, AVG(sc.grade) as average
 FROM courseSection cs 
 JOIN studentCourse sc ON cs.csid = sc.csid
 WHERE (cs.year,cs.semester) not in (select * from CurrentSem) 
-GROUP BY sc.sid
+GROUP BY sc.sid;
 
 CREATE VIEW DepartmentAvg AS
 SELECT d.dcode, MAX(ag.average) as deptavg
 FROM AvgGrades ag
 JOIN student s ON s.sid = ag.sid 
 JOIN department d ON d.dcode = s.dcode
-GROUP BY d.dcode
+GROUP BY d.dcode;
 
 INSERT INTO query5 (dept, sid, sfirstname, slastname, avgGrade)
 (SELECT d.dname as dept, s.sid, s.sfirstname, s.slastname, da.deptavg as avgGrade
 FROM DepartmentAvg da
 JOIN department d ON da.dcode = d.dcode
 JOIN AvgGrades ag ON ag.average = da.deptavg
-JOIN student s ON s.sid = ag.sid)
+JOIN student s ON s.sid = ag.sid);
 
-DROP VIEW CurrentSem;
-DROP VIEW AvgGrades;
 DROP VIEW DepartmentAvg;
-
+DROP VIEW AvgGrades;
 --Query 6
 CREATE VIEW StudentPrereq AS
 SELECT st.sfirstname, st.slastname, c.cname, cs1.year, cs1.semester 
@@ -115,9 +108,9 @@ WHERE (p1.pcid, p1.pdcode) IN (SELECT cs2.cid, cs2.dcode
   JOIN courseSection cs2 ON s2.csid = cs2.csid
   WHERE s1.sid = s2.sid 
   AND ((cs1.year = cs2.year
-  AND cs1.semester > cs2.semester) OR (cs1.year > cs2.year)))
+  AND cs1.semester > cs2.semester) OR (cs1.year > cs2.year)));
 
-INSERT INTO query6 (fname, lname, cname, year, semseter)
+INSERT INTO query6 (fname, lname, cname, year, semester)
 (SELECT st.sfirstname as fname, st.slastname as lname, c.cname, cs.year, cs.semester 
 FROM student st 
 JOIN studentCourse sc ON st.sid = sc.sid
@@ -125,30 +118,17 @@ JOIN courseSection cs ON sc.csid = cs.csid
 JOIN course c ON c.cid = cs.cid
 EXCEPT 
 SELECT * FROM StudentPrereq
-)
+);
 
 DROP VIEW StudentPrereq;
 
 --Query 7
-CREATE VIEW CurrentSem AS
-SELECT year, max(semester) as semester
-FROM courseSection cs
-WHERE year = (
-SELECT max(cs2.year)
-FROM courseSection cs2)
-GROUP BY year
-
-CREATE VIEW csDcode AS
-	SELECT dcode
-	FROM department
-	WHERE dname = 'Computer Science';
-
 CREATE VIEW enrollment3CSAvg AS
 	SELECT cs.cid, cs.dcode, cs.semester, cs.year, avg(sc.grade) as avgmark
 	FROM courseSection cs
 	JOIN studentCourse sc
 		ON cs.csid = sc.csid
-	WHERE dcode in csDcode  AND (cs.year, cs.semester) NOT IN (SELECT * FROM CurrentSem)
+	WHERE dcode in (SELECT * FROM csDcode) AND (cs.year, cs.semester) NOT IN (SELECT * FROM CurrentSem)
 	GROUP BY cs.cid, cs.dcode, cs.semester, cs.year
 	HAVING count(sc.sid) >= 3;
 
@@ -160,7 +140,7 @@ WHERE ca1.avgmark =
   (SELECT MAX(avgmark)
   FROM enrollment3CSAvg ca2
   WHERE ca2.cid = ca1.cid AND ca2.dcode = ca1.dcode
-  GROUP BY ca2.cid, ca2.dcode)
+  GROUP BY ca2.cid, ca2.dcode);
 
 CREATE VIEW MinCourseAverages AS
 SELECT c.cname, ca1.semester, ca1.year, ca1.avgmark
@@ -170,17 +150,17 @@ WHERE ca1.avgmark =
   (SELECT MIN(avgmark)
   FROM enrollment3CSAvg ca2
   WHERE ca2.cid = ca1.cid AND ca2.dcode = ca1.dcode
-  GROUP BY ca2.cid, ca2.dcode)
+  GROUP BY ca2.cid, ca2.dcode);
 
-INSERT INTO query7(cname, semseter, year, avgmark)
+INSERT INTO query7(cname, semester, year, avgmark)
 (SELECT *
 FROM MaxCourseAverages
 UNION 
 SELECT * 
-FROM MinCourseAverages)
+FROM MinCourseAverages);
 
-DROP VIEW csDcode;
-DROP VIEW enrollment3CSAvg;
 DROP VIEW MinCourseAverages;
-DROP VIEW MaxCourseAverages
+DROP VIEW MaxCourseAverages;
+DROP VIEW enrollment3CSAvg;
+DROP VIEW csDcode;
 DROP VIEW CurrentSem;
